91d090e8cc8dd6546a2c74f449f7de4cc495c5d0
avelevon <avelevon23@gmail.com>
2026-01-20 17:44:19 +0400

    upd constants and configs

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,13 +1,13 @@
 import { ethers } from 'hardhat';
 
 export const tokenAddresses = {
-  USDC: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
-  USDT: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+  USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
 };
 
 export const cometTokens = {
-  cUSDC: '0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf',
-  cUSDT: '0xd98Be00b5D27fc98112BdE293e487f8D4cA57d07',
+  cUSDC: '0xc3d688B66703497DAA19211EEdff47f25384cdc3',
+  cUSDT: '0x3Afdc9BCA9213A35503b077a6072F3D0d5AB0840',
 };
 
 export const cometPairs = [
@@ -23,28 +23,40 @@ export const cometPairs = [
 
 export const morphoVaults = [
   {
-    strategy: 'SteakhouseHighYield',
-    vaultAddress: '0x5c0C306Aaa9F877de636f4d5822cA9F2E81563BA',
+    strategy: 'Steakhouse',
+    vaultAddress: '0xBEEF01735c132Ada46AA9aA4c54623cAA92A64CB',
   },
   {
-    strategy: 'YearnDegen',
-    vaultAddress: '0x36b69949d60d06ECcC14DE0Ae63f4E00cc2cd8B9',
+    strategy: 'OEVBoosted',
+    vaultAddress: '0x68Aea7b82Df6CcdF76235D46445Ed83f85F845A3',
+  },
+  {
+    strategy: 'GauntletPrime',
+    vaultAddress: '0xdd0f28e19C1780eb6396170735D45153D261490d',
+  },
+  {
+    strategy: 'GauntletRWA',
+    vaultAddress: '0xA8875aaeBc4f830524e35d57F9772FfAcbdD6C45',
+  },
+  {
+    strategy: 'GauntletFrontier',
+    vaultAddress: '0xc582F04d8a82795aa2Ff9c8bb4c1c889fe7b754e',
   },
   {
     strategy: 'GauntletCore',
-    vaultAddress: '0x7e97fa6893871A2751B5fE961978DCCb2c201E65',
+    vaultAddress: '0x8eB67A509616cd6A7c1B3c8C21D48FF57df3d458',
   },
   {
-    strategy: 'Hyperithm',
-    vaultAddress: '0x4B6F1C9E5d470b97181786b26da0d0945A7cf027',
+    strategy: 'ClearstarHighYield',
+    vaultAddress: '0x9B5E92fd227876b4C07a8c02367E2CB23c639DfA',
   },
 ];
 
-export const ARBITRUM_CHAIN_ID = 42161n;
+export const ETHEREUM_CHAIN_ID = 1n;
 
 export const TREASURY_ADDRESS = process.env.TREASURY_ADDRESS;
 export const WITHDRAW_FEE_PERCENT = BigInt(
-  process.env.WITHDRAW_FEE_PERCENT || '0'
+  process.env.WITHDRAW_FEE_PERCENT || '0',
 );
 export const TIMELOCK_DELAY = Number(process.env.TIMELOCK_DELAY || '1800');
 
